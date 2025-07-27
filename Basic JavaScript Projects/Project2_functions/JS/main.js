2b1d0509fe79488d143520ef2394c962bed8f102
GBDiso81 <Gbdiso81@gmail.com>
2025-07-28 05:13:28 +0800

    project2 assignments

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myfunction() {
+    var var1 = "This is my"; //assigned variable1
+    var var2 = "first Function!"; // assigned variable2
+    document.getElementById("myElement").innerHTML = var1 + " " + var2;
+}
---
+++ Basic JavaScript Projects/Project2_functions/Operator Assignment/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var sentence = "I am learning"; //assigning the variable
+    sentence += " a lot from this book! "; //using operators to add assigned variables
+    document.getElementById("concatenate").innerHTML = sentence; //using getelementbyID
+}
